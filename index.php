fec54284486a44060c203b50435ef1c9985f1049
Diego Arrieta <darrieta.lnx@gmail.com>
2019-11-16 13:16:14 -0300

    tailwind

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Hotel Valdivia</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1></h1>
+    
 </body>
 </html>
