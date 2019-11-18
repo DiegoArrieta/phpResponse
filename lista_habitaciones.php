516160a75b84ed3d7f9b2506ad2404ddb5a14fcd
Diego Arrieta <darrieta.lnx@gmail.com>
2019-11-18 17:15:45 -0300

    lista de habitaciones

---
+++ lista_habitaciones.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
+  // Array
+  $someArray = [
+    [
+      "70900" => ["10", "11","12","13","14","15"],
+      "120900" => ["20","21","22","23","24"],
+      "170900" => ["34","35","36","37","38"]
+
+    ]
+    
+    
+  ];
+
+  // Convert Array to JSON String
+  $someJSON = json_encode($someArray);
+  echo $someJSON;
+?>
