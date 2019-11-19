a3003de3f334dae3a727585395ea7e06d2a05d8c
Diego Arrieta <zeussapp@gmail.com>
2019-11-18 22:50:57 -0300

    Update listado.php

--- listado.php
+++ listado.php
@@ -11,12 +11,14 @@ include 'bd.php';
 $conn = OpenCon();
 $sql = "SELECT * FROM dbcmy8cct16o1bed.reservas";
 $result = $conn->query($sql);
-
+$arreglo = array();
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "id: " . $row["id"]. " - Name: " . $row["nombre"]. " " . $row["apellido"]. "<br>";
+        $arreglo[] = $row['id'], [$row['nombre'], $row['apellido'], $row['fecha_inicio'], $row['fecha_termino'], $row['dias'], $row['servicio'], $row['habitacion '], $row['costo'], $row['pais'];
     }
+    echo $arreglo;
 } else {
     echo "0 results";
 }
