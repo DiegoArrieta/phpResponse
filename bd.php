8627584a87b14f4587d294305781ec3a2d53af09
Diego Arrieta <darrieta.lnx@gmail.com>
2019-11-18 21:49:42 -0300

    some changes

--- bd.php
+++ bd.php
@@ -1,8 +1,15 @@
+
 <?php
-$enlace =  mysql_connect('am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'uqiu9aj1u97yhlc2', 'pieho2sbx0bmh0rv');
-if (!$enlace) {
-    die('No pudo conectarse: ' . mysql_error());
+$servername = "am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "uqiu9aj1u97yhlc2";
+$password = "pieho2sbx0bmh0rv";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-echo 'Conectado satisfactoriamente';
-mysql_close($enlace);
+echo "Connected successfully";
 ?>
